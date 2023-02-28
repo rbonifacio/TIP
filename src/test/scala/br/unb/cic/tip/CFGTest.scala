6f78e3126b49640380091c88e25a6efbce828a98
jose clavo <jclavotafur@gmail.com>
2023-02-28 20:55:07 -0300

    organize tests

--- src/test/scala/br/unb/cic/tip/CFGTest.scala
+++ src/test/scala/br/unb/cic/tip/CFGTest.scala
@@ -7,7 +7,7 @@ import br.unb.cic.tip.*
 
 class CFGTest extends AnyFunSuite {
 
-  test("final of sequence stmt") {
+  test("final of sequence for simple stmts") {
     val s1 = AssignmentStmt("x", ConstExp(1))
     val s2 = AssignmentStmt("y", ConstExp(2))
     val seq = SequenceStmt(s1, s2)
@@ -15,20 +15,25 @@ class CFGTest extends AnyFunSuite {
     assert(Set[Stmt](s2) == finalStmt(seq))
   }
 
-  test("final of sequence if stmt") {
+  test("final of sequence for if stmt") {
+    val s1 = AssignmentStmt("x", ConstExp(1))
+    val s2 = AssignmentStmt("y", ConstExp(2))
+    val s3 = AssignmentStmt("z", ConstExp(3))
+    val s4 = IfElseStmt(EqExp(ConstExp(1),ConstExp(2)), SequenceStmt(s1,s2), None)
+
+    assert(Set[Stmt](s2) == finalStmt(s4))
+  }
+
+  test("final of sequence for if else stmt") {
     val s1 = AssignmentStmt("x", ConstExp(1))
     val s2 = AssignmentStmt("y", ConstExp(2))
     val s3 = AssignmentStmt("z", ConstExp(3))
     val s4 = IfElseStmt(EqExp(ConstExp(1),ConstExp(2)), SequenceStmt(s1,s2), Some(s3))
-    val s5 = IfElseStmt(EqExp(ConstExp(1),ConstExp(2)), SequenceStmt(s1,s2), None)
-
-//    val seq = SequenceStmt(s1, s2)
 
     assert(Set[Stmt](s2, s3) == finalStmt(s4))
-    assert(Set[Stmt](s2) == finalStmt(s5))
   }
 
-  test("cfg stmts") {
+  test("cfg simple stmts") {
     val s1 = AssignmentStmt("x", ConstExp(1))
     val s2 = AssignmentStmt("y", ConstExp(2))
     val seq = SequenceStmt(s1, s2)
@@ -36,21 +41,28 @@ class CFGTest extends AnyFunSuite {
     assert(Set((s1, s2)) == flow(seq))
   }
 
-  test("cfg FACTORIAL") {
+  /**
+      f = 1;
+      while (n>0) {
+        f = f*n;
+        n = n-1;
+      }
+   */
+  test("cfg factorial") {
     val s1 = AssignmentStmt("f", ConstExp(1))
     val s2 = AssignmentStmt("f", MultiExp(VariableExp("f"), VariableExp("n")))
     val s3 = AssignmentStmt("n", SubExp(VariableExp("n"), ConstExp(1)))
     val s4 = SequenceStmt(s2,s3)
     val s5 = WhileStmt(GTExp(VariableExp("n"), ConstExp(0)),s4)
     val s6 = SequenceStmt(s1, s5)
-//    val seq = SequenceStmt(s1, s2)
-    val expexted = Set(
+
+    val expected = Set(
       (s1, s5),
       (s5, s2),
       (s2, s3),
       (s3, s5)
     )
 
-    assert(expexted == flow(s6))
+    assert(expected == flow(s6))
   }
 }
