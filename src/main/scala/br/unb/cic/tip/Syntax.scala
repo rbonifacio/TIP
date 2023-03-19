2a5037546f3352d70c40c8278d4f803128c4957b
jose clavo <jclavotafur@gmail.com>
2023-03-18 22:29:32 -0300

    add comments

--- src/main/scala/br/unb/cic/tip/Syntax.scala
+++ src/main/scala/br/unb/cic/tip/Syntax.scala
@@ -27,36 +27,39 @@ case class FunDecl(name: Id, args: List[Id], vars: List[Id], body: Stmt, retExp:
 
 /** Algebraic data type for expressions */
 enum Expression:
-  case ConstExp(v: Integer) extends Expression
-  case VariableExp(name: Id) extends Expression
-  case AddExp(left: Expression, right: Expression) extends Expression
-  case SubExp(left: Expression, right: Expression) extends Expression
-  case MultiExp(left: Expression, right: Expression) extends Expression
-  case DivExp(left: Expression, right: Expression) extends Expression
-  case EqExp(left: Expression, right: Expression) extends Expression
-  case GTExp(left: Expression, right: Expression) extends Expression
-  case BracketExp(exp: Expression) extends Expression
+  case ConstExp(v: Integer) extends Expression  // 0 | 1 | -1 | 2 | -2 | ...
+  case VariableExp(name: Id) extends Expression // x | y | z | . . .
+  case AddExp(left: Expression, right: Expression) extends Expression // Exp + Exp
+  case SubExp(left: Expression, right: Expression) extends Expression // Exp - Exp
+  case MultiExp(left: Expression, right: Expression) extends Expression // Exp * Exp
+  case DivExp(left: Expression, right: Expression) extends Expression // Exp / Exp
+  case EqExp(left: Expression, right: Expression) extends Expression // Exp == Exp
+  case GTExp(left: Expression, right: Expression) extends Expression // Exp > Exp
+  case BracketExp(exp: Expression) extends Expression // (Exp)
+
   // function-call expressions
-  case DirectFunctionCallExp(name: Id, args: List[Expression]) extends Expression
-  case IndirectFunctionCallExp(exp: Expression, args: List[Expression]) extends Expression
+  case DirectFunctionCallExp(name: Id, args: List[Expression]) extends Expression // Id ( Exp,. . . ,Exp )
+  case IndirectFunctionCallExp(exp: Expression, args: List[Expression]) extends Expression // Exp ( Exp , . . . , Exp )
+
   // pointer-based expressions
-  case AllocExp(exp: Expression) extends Expression
-  case LocationExp(pointer: Id) extends Expression
-  case LoadExp(exp: Expression) extends Expression
-  case NullExp extends Expression
+  case AllocExp(exp: Expression) extends Expression // alloc Exp
+  case LocationExp(pointer: Id) extends Expression // & Id
+  case LoadExp(exp: Expression) extends Expression // * Exp
+  case NullExp extends Expression // null
+
   // record-based expressions
-  case RecordExp(fields: List[Field]) extends Expression
-  case FieldAccess(record: Expression, field: Id)  // pessoa.id
-  case InputExp extends Expression
+  case RecordExp(fields: List[Field]) extends Expression // { Id : Exp , . . . , Id : Exp }
+  case FieldAccess(record: Expression, field: Id)  // Exp . Id
+  case InputExp extends Expression // input
 
 /** Algebraic data type for statements */
 enum Stmt:
-  case AssignmentStmt(name: Id, exp: Expression) extends Stmt
-  case StoreStmt(exp1: Expression, exp2: Expression) extends Stmt
-  case OutputStmt(exp: Expression) extends Stmt
-  case SequenceStmt(s1: Stmt, s2: Stmt) extends Stmt
-  case IfElseStmt(condition: Expression, s1: Stmt, s2: Option[Stmt]) extends Stmt
-  case WhileStmt(condition: Expression, stmt: Stmt) extends Stmt
+  case AssignmentStmt(name: Id, exp: Expression) extends Stmt // Id = Exp
+  case IfElseStmt(condition: Expression, s1: Stmt, s2: Option[Stmt]) extends Stmt // if ( Exp ) { Stmt } [else { Stmt }]
+  case WhileStmt(condition: Expression, stmt: Stmt) extends Stmt // while ( Exp ) { Stmt }
+  case SequenceStmt(s1: Stmt, s2: Stmt) extends Stmt // Stmt Stmt
+  case StoreStmt(exp1: Expression, exp2: Expression) extends Stmt // *Exp = Exp
+  case OutputStmt(exp: Expression) extends Stmt // output Exp
 
 /** Node Types */
 enum Node:
