5ea3d3bc1a3511e2722173fee221c002dd0a65e5
jose clavo <jclavotafur@gmail.com>
2023-03-20 22:53:12 -0300

    function to create path done and its tests

--- src/main/scala/br/unb/cic/tip/Path.scala
+++ src/main/scala/br/unb/cic/tip/Path.scala
@@ -5,16 +5,19 @@ import br.unb.cic.tip.Stmt.*
 import br.unb.cic.tip.Expression.*
 import br.unb.cic.tip.Node.*
 
-type Path = List[Stmt]
+type Path = List[Node]
 
-//def createPath(from: Stmt, to: Stmt, cfg: Graph): Path = to match {
-//  case from => List()
-//  case _ => List(from) //::: successors(from, cfg).toList.map(s => createPath(s, to, cfg))
-//}
-def createPath(from: Stmt, to: Stmt, cfg: Graph): Path = {
-  if (to == from) {
-    List()
-  } else {
-    List(from) ++ successors(from, cfg).toList.map(s => createPath(s, to, cfg)).foldLeft(List())(_ ++ _)
+def path(flow: Graph, function: Id, limit: Int = 2): Set[Path] = {
+  path(StartNode(function), EndNode(function), flow, List(), limit)
+}
+
+def path(from: Node, target: Node, flow: Graph, visited: List[Node], limit: Int): Set[Path] = {
+  var res: Set[Path] = if(from == target) Set(List(from)) else Set()
+
+  val newVisited = from :: visited
+
+  for((n, t) <- flow if (n == from) && (newVisited.filter(p => p == t).size < limit)) {
+    res = res ++ path(t, target, flow, newVisited, limit).map(path => from :: path)
   }
+  res
 }
--- src/test/scala/br/unb/cic/tip/PathTest.scala
+++ src/test/scala/br/unb/cic/tip/PathTest.scala
@@ -7,20 +7,119 @@ import br.unb.cic.tip.Stmt.{AssignmentStmt, *}
 
 class PathTest extends AnyFunSuite {
 
-  test("cfg simple stmts") {
+  /**
+   x = 1
+   z = 3
+   print(z)
+   */
+  test("path_from_stmt") {
     val s1 = AssignmentStmt("x", ConstExp(1))
     val s2 = AssignmentStmt("z", ConstExp(3))
     val s3 = OutputStmt(VariableExp("x"))
-    val seq = SequenceStmt(s1, SequenceStmt(s2, s3))
 
-    val cfg = flow(seq)
+    val body = SequenceStmt(s1, SequenceStmt(s2, s3))
+    val function = FunDecl("f_stmt", List(), List(), body, NullExp)
+    val cfg = flow(function)
 
-    println(createPath(s1, s3, cfg))
+    val paths = path(cfg, function.name)
 
-//    val expected = Set(
-//      (SimpleNode(s1), SimpleNode(s2)),
-//    )
-//
-//    assert(expected == flow(seq))
+    val path1 = List(
+      StartNode(function.name),
+      SimpleNode(s1),
+      SimpleNode(s2),
+      SimpleNode(s3),
+      EndNode(function.name)
+    )
+
+    assert(Set(path1) == paths)
+  }
+
+  /**
+   a = 1
+   if (x == 2) {
+      b = 2
+      c = 3
+   } else {
+    d = 4
+   }
+   e = 5
+   */
+  test("path_from_if_else") {
+    val s1 = AssignmentStmt("a", ConstExp(1))
+    val s2 = AssignmentStmt("b", ConstExp(2))
+    val s3 = AssignmentStmt("c", ConstExp(3))
+    val s4 = AssignmentStmt("d", ConstExp(4))
+    val s5 = IfElseStmt(EqExp(VariableExp("x"),ConstExp(2)), SequenceStmt(s2,s3), Some(s4))
+    val s6 = AssignmentStmt("e", ConstExp(5))
+
+    val body = SequenceStmt(s1, SequenceStmt(s5, s6))
+    val function = FunDecl("f_if_else", List(), List(), body, NullExp)
+    val cfg = flow(function)
+
+    val paths = path(cfg, function.name)
+
+    val path1 = List(
+      StartNode(function.name),
+      SimpleNode(s1),
+      SimpleNode(s5),
+      SimpleNode(s2),
+      SimpleNode(s3),
+      SimpleNode(s6),
+      EndNode(function.name)
+    )
+
+    val path2 = List(
+      StartNode(function.name),
+      SimpleNode(s1),
+      SimpleNode(s5),
+      SimpleNode(s4),
+      SimpleNode(s6),
+      EndNode(function.name)
+    )
+
+    assert(Set(path1, path2) == paths)
+  }
+
+  /**
+    f = 1;
+      while (n>0) {
+        f = f*n;
+        n = n-1;
+      }
+    print(f)
+   */
+  test("path_from_while") {
+    val s1 = AssignmentStmt("f", ConstExp(1))
+    val s3 = AssignmentStmt("f", MultiExp(VariableExp("f"), VariableExp("n")))
+    val s4 = AssignmentStmt("n", SubExp(VariableExp("n"), ConstExp(1)))
+    val s2 = WhileStmt(GTExp(VariableExp("n"), ConstExp(0)), SequenceStmt(s3,s4))
+    val s5 = OutputStmt(VariableExp("f"))
+
+    val body = SequenceStmt(s1, SequenceStmt(s2, s5))
+    val function = FunDecl("f_while", List(), List(), body, NullExp)
+    val cfg = flow(function)
+
+    val paths = path(cfg, function.name)
+
+    val path1 = List(
+      StartNode(function.name),
+      SimpleNode(s1),
+      SimpleNode(s2),
+      SimpleNode(s5),
+      EndNode(function.name)
+    )
+
+    val path2 = List(
+      StartNode(function.name),
+      SimpleNode(s1),
+      SimpleNode(s2),
+      SimpleNode(s3),
+      SimpleNode(s4),
+      SimpleNode(s2),
+      SimpleNode(s5),
+      EndNode(function.name)
+    )
+
+    assert(Set(path1, path2) == paths)
   }
 }
