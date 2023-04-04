7d24a94fbae234837fd15342779cdbe715f3c514
jose clavo tafur <jclavotafur@gmail.com>
2023-04-04 12:54:46 -0300

    change some vars names

--- src/main/scala/br/unb/cic/tip/Path.scala
+++ src/main/scala/br/unb/cic/tip/Path.scala
@@ -7,17 +7,17 @@ import br.unb.cic.tip.Node.*
 
 type Path = List[Node]
 
-def path(flow: Graph, function: Id, limit: Int = 2): Set[Path] = {
-  path(StartNode(function), EndNode(function), flow, List(), limit)
+def path(cfg: Graph, functionName: Id, limit: Int = 2): Set[Path] = {
+  path(StartNode(functionName), EndNode(functionName), cfg, List(), limit)
 }
 
-def path(from: Node, target: Node, flow: Graph, visited: List[Node], limit: Int): Set[Path] = {
-  var res: Set[Path] = if(from == target) Set(List(from)) else Set()
+def path(from: Node, to: Node, cfg: Graph, visited: List[Node], limit: Int): Set[Path] = {
+  var res: Set[Path] = if (from == to) Set(List(from)) else Set()
 
   val newVisited = from :: visited
 
-  for((n, t) <- flow if (n == from) && (newVisited.filter(p => p == t).size < limit)) {
-    res = res ++ path(t, target, flow, newVisited, limit).map(path => from :: path)
+  for((n, t) <- cfg if (n == from) && (newVisited.filter(p => p == t).size < limit)) {
+    res = res ++ path(t, to, cfg, newVisited, limit).map(path => from :: path)
   }
   res
 }
