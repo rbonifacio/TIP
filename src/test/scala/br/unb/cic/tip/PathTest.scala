c1847a5909eddce123193595034f2032bb29b029
jose clavo tafur <jclavotafur@gmail.com>
2023-04-04 12:55:04 -0300

    document tests

--- src/test/scala/br/unb/cic/tip/PathTest.scala
+++ src/test/scala/br/unb/cic/tip/PathTest.scala
@@ -8,9 +8,11 @@ import br.unb.cic.tip.Stmt.{AssignmentStmt, *}
 class PathTest extends AnyFunSuite {
 
   /**
-   x = 1
-   z = 3
-   print(z)
+   s1: x = 1
+   s2: z = 3
+   s3: print(z)
+
+   * Path(1) = s1, s2, s3
    */
   test("path_from_stmt") {
     val s1 = AssignmentStmt("x", ConstExp(1))
@@ -35,24 +37,27 @@ class PathTest extends AnyFunSuite {
   }
 
   /**
-   a = 1
-   if (x == 2) {
-      b = 2
-      c = 3
-   } else {
-    d = 4
-   }
-   e = 5
+   s1: a = 1
+   s2: if (x == 2) {
+   s3:   b = 2
+   s4:   c = 3
+   s-:  } else {
+   s5:    d = 4
+   s-:  }
+   s6: e = 5
+
+    * Path(1) = s1, s2, s3, s4, s6
+    * Path(2) = s1, s2, s5, s6
    */
   test("path_from_if_else") {
     val s1 = AssignmentStmt("a", ConstExp(1))
-    val s2 = AssignmentStmt("b", ConstExp(2))
-    val s3 = AssignmentStmt("c", ConstExp(3))
-    val s4 = AssignmentStmt("d", ConstExp(4))
-    val s5 = IfElseStmt(EqExp(VariableExp("x"),ConstExp(2)), SequenceStmt(s2,s3), Some(s4))
+    val s3 = AssignmentStmt("b", ConstExp(2))
+    val s4 = AssignmentStmt("c", ConstExp(3))
+    val s5 = AssignmentStmt("d", ConstExp(4))
+    val s2 = IfElseStmt(EqExp(VariableExp("x"),ConstExp(2)), SequenceStmt(s3,s4), Some(s5))
     val s6 = AssignmentStmt("e", ConstExp(5))
 
-    val body = SequenceStmt(s1, SequenceStmt(s5, s6))
+    val body = SequenceStmt(s1, SequenceStmt(s2, s6))
     val function = FunDecl("f_if_else", List(), List(), body, NullExp)
     val cfg = flow(function)
 
@@ -61,9 +66,9 @@ class PathTest extends AnyFunSuite {
     val path1 = List(
       StartNode(function.name),
       SimpleNode(s1),
-      SimpleNode(s5),
       SimpleNode(s2),
       SimpleNode(s3),
+      SimpleNode(s4),
       SimpleNode(s6),
       EndNode(function.name)
     )
@@ -71,8 +76,8 @@ class PathTest extends AnyFunSuite {
     val path2 = List(
       StartNode(function.name),
       SimpleNode(s1),
+      SimpleNode(s2),
       SimpleNode(s5),
-      SimpleNode(s4),
       SimpleNode(s6),
       EndNode(function.name)
     )
@@ -81,12 +86,15 @@ class PathTest extends AnyFunSuite {
   }
 
   /**
-    f = 1;
-      while (n>0) {
-        f = f*n;
-        n = n-1;
-      }
-    print(f)
+    s1: f = 1;
+    s2: while (n>0) {
+    s3:   f = f*n;
+    s4:   n = n-1;
+    s-: }
+    s5: print(f)
+
+     * Path(1): s1, s2, s5
+     * Path(2): s1, s2, s3, s4, s2, s5
    */
   test("path_from_while") {
     val s1 = AssignmentStmt("f", ConstExp(1))
