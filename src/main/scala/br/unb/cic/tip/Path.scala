6b848f951e0072912383a2c645b7fef56f0d8938
jose clavo tafur <jclavotafur@gmail.com>
2023-04-04 13:04:41 -0300

    comments

--- src/main/scala/br/unb/cic/tip/Path.scala
+++ src/main/scala/br/unb/cic/tip/Path.scala
@@ -7,10 +7,19 @@ import br.unb.cic.tip.Node.*
 
 type Path = List[Node]
 
+/**
+ * Generate a list of Paths using the CFG from the all program and the name of
+ * a function that already exists and takes on count an specific "deep".
+ * It means the times a path can be re-visited
+ */
 def path(cfg: Graph, functionName: Id, limit: Int = 2): Set[Path] = {
   path(StartNode(functionName), EndNode(functionName), cfg, List(), limit)
 }
 
+/**
+ * Generate a list of Paths from an specific source node to a target node exploring the CFG and
+ * taking on count an specific "deep". It means the times a path can be re-visited
+ */
 def path(from: Node, to: Node, cfg: Graph, visited: List[Node], limit: Int): Set[Path] = {
   var res: Set[Path] = if (from == to) Set(List(from)) else Set()
 
