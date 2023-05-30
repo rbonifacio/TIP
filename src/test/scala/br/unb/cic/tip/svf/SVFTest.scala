8a12904b3256055f14f3c9a77a846e2a3d787e7a
jose clavo tafur <jclavotafur@gmail.com>
2023-05-30 14:57:11 -0300

    test_without_pointer

--- src/test/scala/br/unb/cic/tip/svf/SVFTest.scala
+++ src/test/scala/br/unb/cic/tip/svf/SVFTest.scala
@@ -1,7 +1,8 @@
 package br.unb.cic.tip.svf
 
+import br.unb.cic.tip.{convertSVFtoGraph, exportDot}
 import br.unb.cic.tip.utils.{AssignmentStmt, FunDecl, SequenceStmt}
-import br.unb.cic.tip.utils.Expression.{AddExp, ConstExp, VariableExp, NullExp}
+import br.unb.cic.tip.utils.Expression.{AddExp, ConstExp, NullExp, VariableExp}
 import br.unb.cic.tip.utils.Node.SimpleNode
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -12,17 +13,17 @@ class SVFTest extends AnyFunSuite {
    * s1: a = 1
    * s2: b = 2
    * s3: c = a
-//   * s4: d = b + c
+   * s4: d = b + c
+   * s5: e = c
    */
   test("test_without_pointer") {
     val s1 = AssignmentStmt("a", ConstExp(1))
     val s2 = AssignmentStmt("b", ConstExp(2))
-    val s3 = AssignmentStmt("c", VariableExp("b"))
-//    val s4 = AssignmentStmt("d", AddExp(VariableExp("b"), VariableExp("c")))
+    val s3 = AssignmentStmt("c", VariableExp("a"))
+    val s4 = AssignmentStmt("d", AddExp(VariableExp("b"), VariableExp("c")))
+    val s5 = AssignmentStmt("e", VariableExp("c"))
 
-    //main function
-//    val mainBody = SequenceStmt(s1, SequenceStmt(s2, SequenceStmt(s3, s4)))
-    val mainBody = SequenceStmt(s1, SequenceStmt(s2, s3))
+    val mainBody = SequenceStmt(s1, SequenceStmt(s2, SequenceStmt(s3, SequenceStmt(s4, s5))))
     val mainFunction = FunDecl("main", List(), List(), mainBody, NullExp)
 
     val program = List(mainFunction)
@@ -30,9 +31,13 @@ class SVFTest extends AnyFunSuite {
     val svf = SVF.run(program)
 
     val expected = Set(
-        (VariableExp("b"), VariableExp("c"))
+      (VariableExp("a"), VariableExp("c")),
+      (VariableExp("b"), VariableExp("d")),
+      (VariableExp("c"), VariableExp("d")),
+      (VariableExp("c"), VariableExp("e"))
     )
     assert(expected == svf)
-  }
 
+    println(exportDot(convertSVFtoGraph(svf)))
+  }
 }
