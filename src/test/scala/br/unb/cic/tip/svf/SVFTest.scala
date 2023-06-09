8dab770afc0ce3eae1c9763bce820f48f5620b40
jose clavo tafur <jclavotafur@gmail.com>
2023-06-09 17:28:10 -0300

    test_svf_store_rule_many_allocations

--- src/test/scala/br/unb/cic/tip/svf/SVFTest.scala
+++ src/test/scala/br/unb/cic/tip/svf/SVFTest.scala
@@ -226,4 +226,36 @@ class SVFTest extends AnyFunSuite {
     )
     assert(svf == expected)
   }
+
+  /**
+   * s1: p = alloc i1
+   * s2: q = alloc i2
+   * s3: r = alloc i3
+   * s4: s = alloc i4
+   * s5: p = &q
+   * s6: p = &s
+   * s7: *p = r
+   */
+  test("test_svf_store_rule_many_allocations") {
+    val s1 = AssignmentStmt(PointerExp("p"), AllocExp(ConstExp(1)))
+    val s2 = AssignmentStmt(PointerExp("q"), AllocExp(ConstExp(2)))
+    val s3 = AssignmentStmt(PointerExp("r"), AllocExp(ConstExp(3)))
+    val s4 = AssignmentStmt(PointerExp("s"), AllocExp(ConstExp(4)))
+    val s5 = AssignmentStmt(PointerExp("p"), LocationExp("q"))
+    val s6 = AssignmentStmt(PointerExp("p"), LocationExp("s"))
+    val s7 = AssignmentStmt(LoadExp(PointerExp("p")), PointerExp("r"))
+
+    val mainBody = SequenceStmt(s1, SequenceStmt(s2, SequenceStmt(s3, SequenceStmt(s4, SequenceStmt(s5, SequenceStmt(s6, s7))))))
+    val mainFunction = FunDecl("main", List(), List(), mainBody, NullExp)
+
+    val program = List(mainFunction)
+
+    val svf = SVF.run(program)
+
+    val expected = Set(
+      ((s3, PointerExp("r")), (s7, PointerExp("q"))),
+      ((s3, PointerExp("r")), (s7, PointerExp("s")))
+    )
+    assert(svf == expected)
+  }
 }
